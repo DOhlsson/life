37b76ce38c8d6d08ab08ae0d9ecc1f691a8e26b8
Daniel Ohlsson <dohlsson89@gmail.com>
2021-04-24 08:46:50 +0200

    Using RwLock

--- src/main.rs
+++ src/main.rs
@@ -7,70 +7,74 @@ mod mysdl;
 use crate::mysdl::MySdl;
 use game::Game;
 use std::sync::mpsc::channel;
-use std::sync::{Arc, Barrier};
+use std::sync::{Arc, Barrier, RwLock};
 use std::thread;
 use std::time::Instant;
 
 pub fn main() {
     println!("Hello world!");
 
-    let mut game = Arc::new(Game::new(1000, 1000));
+    let mut game = Arc::new(RwLock::new(Game::new(1000, 1000)));
+    let mut game_clone = game.clone();
 
     let mut sdl = MySdl::start_sdl();
 
     let barrier = Arc::new(Barrier::new(2));
-    let barrier_gameticks = barrier.clone();
+    let barrier_clone = barrier.clone();
 
-    let (tx, rx) = channel();
+    //let (tx, rx) = channel();
 
     thread::spawn(move || {
-        while game.controls().running {
-            tx.send(game.clone()).unwrap();
-            if !game.controls().paused {
+        loop {
+            let game_r = game_clone.read().unwrap();
+            let paused = game_r.controls().paused;
+
+            std::thread::sleep(std::time::Duration::from_millis(game_r.speed()));
+
+            if !game_r.controls().running {
+                break;
+            }
+
+            if !paused {
                 let timer_tick = Instant::now();
-                game.tick();
+                game_r.tick();
                 let time_tick = timer_tick.elapsed().as_millis();
                 println!("Tick took {}", time_tick);
             }
 
-            barrier_gameticks.wait(); // wait for render thread to complete render
-            barrier_gameticks.wait(); // wait for render thread to complete handling events
+            drop(game_r);
 
-            if !game.controls().paused {
-                let game = Arc::get_mut(&mut game).unwrap();
-                game.finalize_tick();
+            if !paused {
+                let mut game_w = game_clone.write().unwrap();
+                game_w.finalize_tick();
             }
 
-            barrier_gameticks.wait();
-
-            std::thread::sleep(std::time::Duration::from_millis(game.speed()));
+            barrier_clone.wait(); // lockstep
         }
     });
 
-    'running: loop {
-        let game = rx.recv().unwrap();
+    loop {
+        let game_r = game.read().unwrap();
 
         let timer = Instant::now();
-        game.draw(&mut sdl);
+        game_r.draw(&mut sdl);
         let time_draw = timer.elapsed().as_millis();
         println!("Draw took {}", time_draw);
 
-        if !game.controls().running {
-            break 'running;
+        if !game_r.controls().running {
+          break;
         }
 
-        barrier.wait(); // wait for tick
+        game_r.handle_events(&mut sdl);
+        drop(game_r);
 
-        game.handle_events(&mut sdl);
-        drop(game);
 
-        barrier.wait(); // we are done
-
-        barrier.wait(); // wait for tick to finalize
+        barrier.wait(); // lockstep
 
         let time_all = timer.elapsed().as_millis();
-
         println!("All took {}", time_all);
+
+
 
         // TODO: limit to 60 fps
     }
